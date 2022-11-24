72bfe91c9d3aea68667b50cf9ad79b59f3d6843d
Ashlyn Knox <ashlyn@lilyx.io>
2022-11-24 13:03:08 -0700

    card project setup

---
+++ components/AppCard.vue
@@ -0,0 +1,11 @@
+<script setup></script>
+<template>
+  <article class="max-w-sm bg-slate-200 p-4 rounded-md">
+    <h3>Title</h3>
+    <p>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nostrum,
+      suscipit!
+    </p>
+    <NuxtLink to="#">Button</NuxtLink>
+  </article>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,15 @@
 <template>
-  <main>
-    <h1>Home Page</h1>
+  <main class="mx-auto">
+    <header
+      class="container flex justify-center text-5xl font-bold my-8 text-center"
+    >
+      <h1>Home Page</h1>
+    </header>
+    <section>
+      <header class="text-3xl text-center py-4 font-semibold">
+        <h2>Card Layout</h2>
+      </header>
+      <AppCard />
+    </section>
   </main>
 </template>
