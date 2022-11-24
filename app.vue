135fdf8b46b8e22248251ffcd1a47fc38054cae4
Ashlyn Knox <ashlyn@lilyx.io>
2022-11-24 10:19:38 -0700

    initial setup

---
+++ app.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <nav>
+      <NuxtLink to="/">Home</NuxtLink>
+      <NuxtLink to="about">About</NuxtLink>
+    </nav>
+    <!-- Page Content -->
+    <NuxtPage />
+    <footer>
+      <p>&copy; 2022 SAIT Web Developers</p>
+    </footer>
+  </div>
+</template>
