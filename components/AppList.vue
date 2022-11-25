0a325e2af8e7a5b7d72929f1abf17fd40638d22e
Ashlyn Knox <ashlyn@lilyx.io>
2022-11-25 10:53:38 -0700

    list component with conditionals

---
+++ components/AppList.vue
@@ -0,0 +1,24 @@
+<script setup>
+defineProps({
+  ordered: {
+    type: Boolean,
+    default: false,
+  },
+  bullets: {
+    type: Boolean,
+    default: false,
+  },
+});
+</script>
+<template>
+  <div class="container my-4 mx-4 max-w-lg md:mx-12">
+    <!-- if ordered == true use this -->
+    <ol v-if="ordered" class="list-decimal">
+      <slot></slot>
+    </ol>
+    <!-- otherwise use this -->
+    <ul v-else :class="bullets ? 'list-disc' : 'list-none'">
+      <slot></slot>
+    </ul>
+  </div>
+</template>
--- pages/about.vue
+++ pages/about.vue
@@ -1,5 +1,45 @@
+<script setup>
+const kotor = [
+  {
+    name: "Revan",
+    description: "Dark Lord with a sweet mask, turncoat Republic savior.",
+  },
+  {
+    name: "Malek",
+    description: "Dark Lord with a gross face, Republic scourge",
+  },
+  {
+    name: "Bastila",
+    description:
+      "Jedi with a cool double edged lightsaber and amazing Battle Meditation skills.",
+  },
+];
+</script>
 <template>
   <AppMain>
     <PageHeader title="About Page" />
+    <AppSection>
+      <SectionHeader title="List Component" />
+    </AppSection>
+    <!-- List Examples-->
+    <div class="flex space-around gap-4 flex-wrap">
+      <section>
+        <h3>Unordered List</h3>
+        <AppList bullets>
+          <AppListItem v-for="jedi in kotor" :key="jedi.id" brand>
+            <h4 class="text-xl font-semibold">{{ jedi.name }}</h4>
+            <p class="text-black">{{ jedi.description }}</p>
+          </AppListItem>
+        </AppList>
+      </section>
+      <section>
+        <h3>Ordeded List</h3>
+        <AppList ordered>
+          <li>one</li>
+          <li>two</li>
+          <li>three</li>
+        </AppList>
+      </section>
+    </div>
   </AppMain>
 </template>
