c534f24c76641b09442741d3c63c3080362252ac
Ashlyn Knox <ashlyn@lilyx.io>
2022-11-25 11:18:50 -0700

    added metadata and font import

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,29 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: [
-    '@nuxtjs/tailwindcss'
-  ]
-})
+  app: {
+    head: {
+      meta: [
+        { name: "title", content: "Sait Nuxt 3 App" },
+        {
+          name: "description",
+          content: "A place to learn and practice using Nuxt 3",
+        },
+      ],
+      link: [
+        {
+          rel: "preconnect",
+          href: "https://fonts.googleapis.com",
+        },
+        {
+          rel: "preconnect",
+          href: "https://fonts.googleapis.com",
+        },
+        {
+          rel: "stylesheet",
+          href: "https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Bitter:wght@100&display=swap",
+        },
+      ],
+    },
+  },
+  modules: ["@nuxtjs/tailwindcss"],
+});
