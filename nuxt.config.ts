7822003bb12ccad6d649081ba4c5fa5b3e7c09df
Ashlyn Knox <ashlyn@lilyx.io>
2022-11-24 22:52:29 -0700

    google fonts configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,30 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  typescript: {
-    shim: false,
+  app: {
+    head: {
+      title: "SAIT Nuxt App",
+      meta: [
+        {
+          name: "description",
+          content: "Learning Web Development with Nuxt",
+        },
+      ],
+      link: [
+        {
+          rel: "preconnect",
+          href: "https://fonts.googleapis.com",
+        },
+        {
+          rel: "preconnect",
+          href: "https://fonts.gstatic.com",
+          crossorigin: "",
+        },
+        {
+          rel: "stylesheet",
+          href: "https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Bitter:wght@100&display=swap",
+        },
+      ],
+    },
   },
   modules: ["@nuxtjs/tailwindcss"],
 });
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  theme: {
+    fontFamily: {
+      sans: ['"Bebas Neue"', 'sans-serif'],
+      serif: ['Bitter', 'serif'],
+    }
+  },
+}
